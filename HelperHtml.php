7d016d2dea12b6eea525329842339bc18ffb3180
Joeyybad <98761238+Joeyybad@users.noreply.github.com>
2022-02-01 11:46:07 +0100

    Add files via upload

---
+++ HelperHtml.php
@@ -0,0 +1,10 @@
+<?php
+// fonction qui vont permetttre de génerer une partie de la page HTML
+function renderNavbar(){
+  return "
+  <nav>
+    <a href ='index.php'> Accueil </a>
+    <a href ='contact.php'> Me contacter </a>
+  </nav>
+";   
+}
