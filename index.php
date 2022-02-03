3ced4dbca217ca22e68f705f046bfbd8f6295ffa
JoeyyBad <jnkunga@hotmail.com>
2022-02-03 12:37:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php require 'HelperHtml.php' ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name ="viewport" content ="width=device-width, initial-scale = 1.0">
+    <meta http-equiv = "X-UA-Compatible" content ="IE = edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Style.css">
+  </head>
+  
+  <body>
+    <header>
+      <h1> Mon site </h1>
+      <?php echo renderNavbar(); ?>
+    </header>
+<p> git test </p>
+    
+
+  </body>
+</html>
+
+  
